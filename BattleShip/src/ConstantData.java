0bac6c5b0a1f6e7a6d80adbc3e7507187949c49b
rob0229 <rob0229@gmail.com>
2014-12-29 09:16:08 -0500

    Minor code cleanup

--- BattleShip/src/ConstantData.java
+++ BattleShip/src/ConstantData.java
@@ -16,20 +16,6 @@ public class ConstantData {
 	public static String CRUISERVERT = "img/Cruiser_r.png";
 	public static String DESTROYERVERT = "img/Destroyer_r.png";
 	public static String SUBMARINEVERT = "img/Submarine_r.png";
-
-	/*
-	public static String BATTLESHIP = "img/Battleship.png";
-	public static String CARRIER = "img/Carrier.png";
-	public static String CRUISER = "img/Cruiser.png";
-	public static String DESTROYER = "img/Destroyer.png";
-	public static String SUBMARINE = "img/Submarine.png";
-	
-	public static String BATTLESHIPVERT = "img/Battleship_r.png";
-	public static String CARRIERVERT = "img/Carrier_r.png";
-	public static String CRUISERVERT = "img/Cruiser_r.png";
-	public static String DESTROYERVERT = "img/Destroyer_r.png";
-	public static String SUBMARINEVERT = "img/Submarine_r.png";
-	*/
 	
 	public static String HITLABEL = "img/hitLabel.gif";
 	public static String MISSLABEL = "img/missLabel.jpg";
