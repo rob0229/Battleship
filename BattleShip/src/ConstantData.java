19d610903a2f8d24b4128b13a6160007e49d9095
Rob <rob0229@gmail.com>
2014-12-28 23:34:21 -0500

    Update ConstantData.java
    Removed src/ from ship image URLs

--- BattleShip/src/ConstantData.java
+++ BattleShip/src/ConstantData.java
@@ -5,17 +5,17 @@
  */
 
 public class ConstantData {
-	public static String BATTLESHIP = "src/img/Battleship.png";
-	public static String CARRIER = "src/img/Carrier.png";
-	public static String CRUISER = "src/img/Cruiser.png";
-	public static String DESTROYER = "src/img/Destroyer.png";
-	public static String SUBMARINE = "src/img/Submarine.png";
+	public static String BATTLESHIP = "img/Battleship.png";
+	public static String CARRIER = "img/Carrier.png";
+	public static String CRUISER = "img/Cruiser.png";
+	public static String DESTROYER = "img/Destroyer.png";
+	public static String SUBMARINE = "img/Submarine.png";
 	
-	public static String BATTLESHIPVERT = "src/img/Battleship_r.png";
-	public static String CARRIERVERT = "src/img/Carrier_r.png";
-	public static String CRUISERVERT = "src/img/Cruiser_r.png";
-	public static String DESTROYERVERT = "src/img/Destroyer_r.png";
-	public static String SUBMARINEVERT = "src/img/Submarine_r.png";
+	public static String BATTLESHIPVERT = "img/Battleship_r.png";
+	public static String CARRIERVERT = "img/Carrier_r.png";
+	public static String CRUISERVERT = "img/Cruiser_r.png";
+	public static String DESTROYERVERT = "img/Destroyer_r.png";
+	public static String SUBMARINEVERT = "img/Submarine_r.png";
 
 	/*
 	public static String BATTLESHIP = "img/Battleship.png";
